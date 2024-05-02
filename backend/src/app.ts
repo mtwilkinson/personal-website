3c5261f971a7e89169ee5c4e9411bb5fd7adb488
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-05-02 13:42:20 -0400

    set turbo to run in one repo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import example from "./routes/example";
-
 const app = express();
 
 app.use(express.json());
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Prisma } from "@prisma/client";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    async function postData() {
+
+        const data: Prisma.FeedbackCreateInput = {
+            name: 'Mike',
+            feedback: 'is an SA'
+        }
+        //sends a post request the /api/high-score
+        const res = await axios.post("/api/example", data);
+        if(res.status === 200) {
+            console.log("added feedback");
+        }
+    }
+
+    async function getData() {
+        const res = await axios.get("api/example");
+        console.log(res.data);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={postData}>post feedback</button>
+            <button onClick={getData}>get feedback</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
