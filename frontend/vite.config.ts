3b0adae3dbe5677add1a11daf07bf970c0e7470c
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-05-02 18:41:38 -0400

    vercel please

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
     proxy: {
-      "/api": "http://localhost" + ":" + "3001",
+      "/api": process.env.BACKEND_SOURCE + ":" + process.env.BACKEND_PORT,
     },
   },
   build: {
